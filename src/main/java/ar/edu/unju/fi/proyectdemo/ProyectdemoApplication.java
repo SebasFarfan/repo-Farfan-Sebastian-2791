3acd4c1aad232baad039f88ed5d77c269dfa1344
Sebas <sebas.654@gmail.com>
2022-04-06 01:20:04 -0300

    inicio proyecto

---
+++ src/main/java/ar/edu/unju/fi/proyectdemo/ProyectdemoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.proyectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/fi/proyectdemo/ProyectdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.proyectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
